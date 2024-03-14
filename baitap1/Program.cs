1ad4e53d98c3f6daad879bcc3a84b551b85ab402
Admin <Admin@DESKTOP-EKO5MQF>
2024-03-14 17:18:30 +0700

    Add project files.

---
+++ baitap1/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitap1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("bai tap 1");
+            //int n;
+            //Console.WriteLine("nhap vao so nguyen duong n");
+            //n=int.Parse(Console.ReadLine());
+            //if (n% 3 == 0) 
+            //{
+            //    Console.WriteLine(" n la so chia het cho 3");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("n la so khong chia het cho 3");
+            //}
+
+            //Console.ReadKey();
+
+
+
+            //Console.WriteLine("bai tap 2");
+            //int n;
+            //Console.WriteLine("nhap vao so n bang cuu chuong");
+            //n=int.Parse(Console.ReadLine());
+            //for(int i = 1; i <= n; i++) 
+            //{
+            //    Console.WriteLine("cuu chuong {0} la", i);
+            //    for (int j = 1; j <= 10; j++)
+            //    {
+            //        Console.WriteLine("{0} x {1} = {2}", i, j, (i * j));
+            //    }
+            //    Console.WriteLine("--- Ket thuc cuu chuong ---");
+
+            //}
+            //Console.ReadKey();
+
+
+            //Console.WriteLine("bai tap 3");
+            //int n, giaithua = 1;
+            //Console.WriteLine("nhap vao so nguyen n can tinh giai thua: ");
+            //n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    giaithua *= i;
+
+            //}
+            //Console.WriteLine("giai thua cua {0} la {1}", n, giaithua);
+            //Console.ReadKey();
+
+
+
+            //Console.OutputEncoding = Encoding.UTF8;
+            //Console.WriteLine("bai tap 4");
+            //int n;
+            //Console.WriteLine("nhap vao so can kiem tra: ");
+            //n = int.Parse(Console.ReadLine());
+            //int sqr = (int)Math.Sqrt(n);
+            //if (sqr * sqr == n)
+            //{
+            //    Console.WriteLine("{0} là số chính phương", n);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("{0} không phải là số chính phương", n);
+            //}
+            //Console.ReadKey();
+
+
+            //Console.OutputEncoding = Encoding.UTF8;
+            //Console.WriteLine("bai tap 5");
+            //Console.WriteLine("nhập vào số tháng bất kỳ:");
+            //int thang=int.Parse(Console.ReadLine());
+            //switch (thang) 
+            //{
+            //    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+            //        Console.WriteLine("tháng {0} có 31 ngày", thang);
+            //        break;
+
+            //    case 4: case 6: case 9: case 11:
+            //        Console.WriteLine("Tháng {0} có 30 ngày", thang);
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("tháng {0} có 28 ngày hoặc 31 ngày", thang);
+            //        break;
+            //    default:
+            //        Console.WriteLine("Tháng không hợp lệ");
+            //        break;
+
+            //}
+            //Console.ReadKey();
+
+
+
+            //Console.OutputEncoding = Encoding.UTF8;
+            //Console.WriteLine("bai tap 6");
+            //int n;
+            //long S = 0;
+            //Console.WriteLine(" moi ban nhap vao so n ");
+            //n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    S+= (int)Math.Pow(i, i);
+            //}
+            //Console.WriteLine(" tong S là {0}", S);
+            //Console.ReadKey();
+
+
+
+            //Console.OutputEncoding = Encoding.UTF8;
+            //Console.WriteLine("bai tap 7");
+            //Console.WriteLine("nhập vào số cần tính");
+            //int n = int.Parse(Console.ReadLine());
+            //int sum = 0;
+            //for (int i = 1; i <= n; i+=2) 
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine("tong của các số lẻ từ 1 đến {0} là {1}", n, sum);
+            //Console.ReadKey();
+
+
+
+            //Console.WriteLine("bai tap 8");
+            //Console.WriteLine("nhap vao so n: ");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 0; i <= n; i++)
+            //{
+            //    int Counter = 0;
+            //    for (int j = 2; j <= i / 2; j++)
+            //    {
+            //        if (i % j == 0)
+            //        {
+            //            Counter++;
+            //            break;
+            //        }
+
+            //    }
+            //    if (Counter == 0 && i != 1)
+            //    {
+            //        Console.WriteLine("{0}", i);
+            //    }
+            //}
+            //Console.ReadKey();
+
+
+
+            //Console.WriteLine("bai tap 9");
+            //Console.Write("Nhap vao so hang: ");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <=n; i++) 
+            //{
+            //    for (int j = 1; j <=i; j++) 
+            //    {
+            //        Console.Write("*");
+
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.ReadKey();
+
+
+
+            //Console.WriteLine("bai tap 10");
+            //int a=0; int b = 1; int c;
+            //Console.WriteLine(" nhap vao so n");
+            //int n =int.Parse(Console.ReadLine());
+            //Console.WriteLine("day 10 so la");
+            //Console.WriteLine("{0}   {1}", a, b);
+
+            //for (int i = 3; i <=n; i++)
+            //{
+            //    c = a + b;
+            //    Console.WriteLine("{0}", c);
+            //    a = b;
+            //    b = c;
+
+            //}
+            //Console.WriteLine();
+
+            //Console.ReadKey();
+        }
+    }
+}
